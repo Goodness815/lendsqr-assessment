692cb492f1b14f410355a363229b6520afd67e4d
Goodness Sewo <sewogoodness111@gmail.com>
2026-06-25 17:41:21 +0100

    feat: update user profile name and add mobile responsive styling to Topbar component

--- src/components/topbar/Topbar.tsx
+++ src/components/topbar/Topbar.tsx
@@ -53,7 +53,7 @@ export default function Topbar({ sidebarOpen, onToggle }: TopbarProps) {
                 <path d="M12 12c2.7 0 4.8-2.1 4.8-4.8S14.7 2.4 12 2.4 7.2 4.5 7.2 7.2 9.3 12 12 12zm0 2.4c-3.2 0-9.6 1.6-9.6 4.8v2.4h19.2v-2.4c0-3.2-6.4-4.8-9.6-4.8z" />
               </svg>
             </div>
-            <span className={s.name}>Adedeji</span>
+            <span className={s.name}>Goodness Sewo</span>
             <ChevronDown size={16} className={s.chevron} />
           </button>
 
